2cf1e969030c37a95a4dfe33051e3718732a58e3
USER <USER@ASHIYANA>
2020-06-27 20:56:13 +0530

    Assignment 1

---
+++ AreaRectangle/src/area/Rectangle.java
@@ -0,0 +1,13 @@
+package area;
+
+public class Rectangle {
+
+	public static void main(String[] args) {
+		float l =3.4f;
+		float b =6.7f;
+		float A = l*b;
+		System.out.println("Area of rectangle: " +A);
+
+	}
+
+}
