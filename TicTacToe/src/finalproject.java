44c953a9ff0588909447e434f56c231834900206
USER <USER@ASHIYANA>
2020-06-27 21:01:29 +0530

    Final project (Assignment 3)

---
+++ TicTacToe/src/finalproject.java
@@ -0,0 +1,22 @@
+
+public class finalproject {
+	public static void main(String[]args) {
+		System.out.println("Welcome to 2 player Tic Tac Toe.");
+		System.out.println("--------------------------------");
+		System.out.println("/---|---|---\\");
+		System.out.println("| 1 | 2 | 3 |");
+		System.out.println("|-----------|");
+		System.out.println("| 4 | 5 | 6 |");
+		System.out.println("|-----------|");
+		System.out.println("| 7 | 8 | 9 |");
+		System.out.println("/---|---|---\\");
+		System.out.println("X's will play first. Enter a slot number to place X in: ");
+	
+		
+		
+		
+		
+		
+	}
+
+}
